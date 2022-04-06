69c8f9e14d2ab0454b825956b0f0d38e5ecc2579
Gacem bz <gacem.bouzakoura10@gmail.com>
2022-04-06 15:09:20 +0200

    fe

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import img from "./img2.jfif";
+function App() {
+  return (
+    <section style={{ border: "1px solid black", maxWidth: "100vw" }}>
+      <h1 className="name">gacem</h1>
+      <div>
+        <img src="/img1.jpg" alt="icon" />
+        <br></br>
+        <img src={img} alt="zadazd" />
+        <video width="320" height="240" controls>
+          {" "}
+          <source src="/vid.mp4" type="video/mp4"></source>
+        </video>
+      </div>
+    </section>
+  );
+}
+
+export default App;
